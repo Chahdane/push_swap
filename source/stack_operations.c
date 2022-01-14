e0227977905cf98e142f896c53d131587010b8ab
Chahdane <a.chahdane@gmail.com>
2022-01-14 15:19:37 +0100

    adding rr/rrr func

--- source/stack_operations.c
+++ source/stack_operations.c
@@ -70,12 +70,16 @@ void	rotate(stack **top, char ab)
 }
 /* used tp shift down all elements of stack a by 1.
 	The last element becomes the first one */
-	
+
 void	reverse_rotate(stack **top, char ab)
 {
 	stack *before_last;
 	stack *last;
 
+	if (!top || !(*top)->next)
+		return ;
+	if (ab == 'a' || ab == 'b')
+		printf("rr%c\n",ab);
 	before_last = *top;
 	while(before_last->next->next != NULL)
 		before_last = before_last->next;
@@ -83,17 +87,29 @@ void	reverse_rotate(stack **top, char ab)
 	last->next = *top;
 	before_last->next = NULL;
 	*top = last;
-}	
-void rr(stack **a, stack **b)
-{
-	printf("rr");
-	rotate(a,'0');
-	rotate(b,'0');
 }
+
+// this function is used to do rr || rrr operations
+
+void rr(stack **a, stack **b, char is_reverse)
+{
+	if (is_reverse == 'n')
+	{
+		printf("rr");
+		rotate(a,'0');
+		rotate(b,'0');
+	}
+	else if (is_reverse == 'y')
+	{
+		printf("rr");
+		reverse_rotate(a,'0');
+		reverse_rotate(b,'0');
+	}
+}
+
 void ss(stack *a, stack *b)
 {
 	printf("ss");
 	swap(a,'0');
 	swap(b,'0');
 }
-
