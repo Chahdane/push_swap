2de06d18b55fc4bdfb66e738afb6d2ec70191f3c
Chahdane <a.chahdane@gmail.com>
2022-01-15 13:46:25 +0100

    OK

--- source/sort_5_elements.c
+++ source/sort_5_elements.c
@@ -1,29 +1,32 @@
 #include"../includes/push_swap.h"
-void sort_5_elements(stack **top)
+
+void	sort_5_elements(stack **top)
 {
-	stack *b;
-	int i;
+	stack	*b;
+	int		i;
 
 	i = 0;
 	b = NULL;
 	push2_sort3(top, &b);
 	while (i < 2)
 	{
-		if (b->data <= (*top)->data)						//insert at first pos
+		if (b->data <= (*top)->data)
 			push_element(&b, top, 'a');
-		else if (b->data > (*top)->data)					
+		else if (b->data > (*top)->data)
 		{
-			if (b->data <= (*top)->next->data)				// 2nd pos
+			if (b->data <= (*top)->next->data)
 				push_to_pos2(top, &b);
-			else if (b->data < (*top)->next->next->data)	// 3rd pos
+			else if (b->data < (*top)->next->next->data)
 				push_to_pos3(top, &b);
-			else 											//last pos
+			else
 				push_to_pos4(top, &b);
 		}
 		i++;
 	}
 }
-// pushs 2 elemnts to stack B, then sort the 3 in the stack A
+/*  push 2 elemnts from A to B,
+	then sort the 3 in the stack A */
+
 void	push2_sort3(stack **top, stack **b)
 {
 	push_element(top, b, 'b');
@@ -31,29 +34,33 @@ void	push2_sort3(stack **top, stack **b)
 	sort_3_elements(top);
 }
 
-// push an element to 2nd pos (1 -> a -> (2) -> b -> 3 c -> 4) 
+/* push the first element of b to 2nd pos of a
+	(1 -> a -> (2) -> b -> 3 -> c -> 4)  */
 
 void	push_to_pos2(stack **top, stack **b)
 {
 	rotate(top, 'a');
-	push_element(b,top,'a');
+	push_element(b, top, 'a');
 	reverse_rotate(top, 'a');
 }
 
-// push an element to 2nd pos (1 -> a -> 2 -> b -> (3) c -> 4) 
+/* push the first element of b to 3rd pos of a
+	(1 -> a -> 2 -> b -> (3) -> c -> 4)  */
 
-void	push_to_pos3(stack **top , stack **b)
+void	push_to_pos3(stack **top, stack **b)
 {
 	rotate(top, 'a');
 	rotate(top, 'a');
-	push_element(b,top,'a');
+	push_element(b, top, 'a');
 	reverse_rotate(top, 'a');
 	reverse_rotate(top, 'a');
 }
-// push an element to 2nd pos (1 -> a -> 2 -> b -> 3 c -> (4)) 
+
+/* push the first element of b to 4 pos of a
+	(1 -> a -> 2 -> b -> 3 c -> (4))  */
 
 void	push_to_pos4(stack **top, stack **b)
 {
-	push_element(b,top,'a');
+	push_element(b, top, 'a');
 	rotate(top, 'a');
 }
